54224d4e392de1761674e2e26af51283ce75fdc2
Kuldip <kuldeepchudasama6999@gmail.com>
2024-12-14 02:04:52 +0530

    2.18

--- Client/src/pages/Result.jsx
+++ Client/src/pages/Result.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { useState } from "react";
+import { assets } from "../assets/assets";
 
 function Result() {
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const[loading,setLoding] = useState(false)
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form
+      action=""
+      className="flex flex-col min-h-[90vh] justify-center items-center"
+    >
+      <div>
+        <div className="relative">
+          <img src={assets.sample_img_1} className="max-w-sm rounded" alt="" />
+          <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]"></span>
+        </div>
+        <p className={!loading ? "hidden" : ""}>Loading...</p>
+      </div>
+
+      {!imageLoaded && (
+        <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+          <input
+            type="text"
+            placeholder="Describe what you want to generate"
+            className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
+          />
+          <button
+            type="submit"
+            className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full"
+          >
+            Generate
+          </button>
+        </div>
+      )}
+
+      {imageLoaded && (
+        <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+          <p className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">
+            Generate Another
+          </p>
+          <a href={image} className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">Download</a>
+        </div>
+      )}
+    </form>
+  );
 }
 
-export default Result
+export default Result;
